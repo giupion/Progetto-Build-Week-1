04758b8d3973a12763bf0a54e364ae09e6166fcb
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-11-16 15:03:12 +0100

    esame superato done

--- pagina 3-results/js/script.js
+++ pagina 3-results/js/script.js
@@ -7,34 +7,55 @@
 
 
 
-  
+ 
   let nodo = document.querySelector('#risultati').getContext('2d');
-  
+ 
 
  let graficoCiambella = new Chart(nodo, {
     type: 'doughnut',
    
     data : {
-      
+      labels: [
+        'Wrong',
+        'Correct',
+        
+      ],
       datasets: [{
         data: [33.3,66.7], 
         backgroundColor: ["#C1138C","#03FFFF" ] 
       }]
     },
-    
+   
     options:{
+
+      
       cutoutPercentage: 50, 
       responsive: true, 
-      cutout: 150,
+      cutout: 140,
+
+      elements: {
+
+        
+        arc: {
+          borderWidth: 1, // <-- Set this to derired value
+          borderColor:'#333'
+        }
+      },
+    
+      plugins: {
+        legend: false,
+    }
+
     },
-   
-  
-  });
 
-
-  
+    
+  }
  
-  console.dir(Chart)
+  );
+   
+
+ 
+   console.log(graficoCiambella)
 
 
   
