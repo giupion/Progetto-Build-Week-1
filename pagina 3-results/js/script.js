3d70f29ff051cbb9b706f52ed37ee1540b28b6cd
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-11-16 13:29:10 +0100

    aggiornamento testo donut

--- pagina 3-results/js/script.js
+++ pagina 3-results/js/script.js
@@ -1,24 +1,43 @@
 
-let data = {
-   
-    datasets: [{
-      data: [33.3,66.7], 
-      backgroundColor: ["#C1138C","#03FFFF" ] 
-    }]
-  };
 
   
-let options = {
-    cutoutPercentage: 50, 
-    responsive: true, 
-  };
+
+
+  
+
+
 
   
   let nodo = document.querySelector('#risultati').getContext('2d');
+  
 
  let graficoCiambella = new Chart(nodo, {
     type: 'doughnut',
-    data: data,
-    options: options
+   
+    data : {
+      
+      datasets: [{
+        data: [33.3,66.7], 
+        backgroundColor: ["#C1138C","#03FFFF" ] 
+      }]
+    },
+    
+    options:{
+      cutoutPercentage: 50, 
+      responsive: true, 
+      cutout: 150,
+    },
+   
+  
   });
-  chart();
+
+
+  
+ 
+  console.dir(Chart)
+
+
+  
+  /*label:`Congratulations!
+   You passed the exam.`
+   `We'll send you the certificate in few minutes.Check your email (including promotions/spam folder)`,*/
