eaa2ef2c6f646a9f65a95242f6b29650c831a145
Victor <victor246257@gmail.com>
2023-11-17 14:10:56 +0100

    modifiche pag4

--- pagina 4-review/js/scriptPag4.js
+++ pagina 4-review/js/scriptPag4.js
@@ -2,12 +2,21 @@
 
 let stellina = document.querySelectorAll("span")
 console.log(stellina)
-
 for(let i =0;i<stellina.length;i++){
-    stellina[i].addEventListener("click",()=>{
+    stellina[i].addEventListener("mouseover",()=>{
+        for(let l=i;l>=0;l--){    
+        stellina[l].className="stellina2"}
+        })
+    stellina[i].addEventListener("mouseout",() => {
+        for(let k=i;k>=0;k--){
+        stellina[k].className = "stellina"}
+    })
+    stellina[i].addEventListener("click",()=>{     
         for(let c=i;c>=0;c--){
             stellina[c].style.color= "#00FFFF"
         }
-
+        for(let j=i+1;j>=i;j++){
+            stellina[j].style.color=" rgb(0, 0, 0,0.6)"
+        }
     })
 }
