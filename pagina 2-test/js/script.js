de4af67697bd630faed2a95abe3d0c0e146dde91
Gregorio Vecchio <gregoriovecchio@gmail.com>
2023-11-17 11:13:37 +0100

    pushata timerina

--- pagina 2-test/js/script.js
+++ pagina 2-test/js/script.js
@@ -1,4 +1,56 @@
 
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+const TIME_LIMIT = 20;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<button type="button">Inizia il countodown</button>
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+document.querySelector("button").addEventListener("click",()=>{
+   startTimer();
+});
+
+
 const questions = [
     {
         category: "Science: Computers",
@@ -207,56 +259,73 @@ function valutaRisposta(){
 
 
 
-/*
-        QUIZ GAME!
-
-        REGOLE:
-        / L'utente dovrà indovinare un certo numero di domandeThe player must guess correctly a certain amount of questions
-        / Ogni risposta corretta gli darà 1 punto
-        / Le domande possono avere risposte multiple o singole (true/false)
-        / Al termine del quiz l'utente dovrà poter vedere il suo punteggio
-
-        TIPS:
-        / Usa una variabile globale per registrare il punteggio dell'utente
-        / Crea una variabile "questionNumber" per tenere traccia del numero (o posizione) della domanda presentata all'utente
-        / Quando "questionNumber" è maggiore delle domande disponibili, a quel punto l'applicazione dovrà mostrare il punteggio
-        / Comincia salvando le domande in una variabile
-        / Fai test completi: controlla la console periodicamente per verificare che non ci siano errori e che il flusso di dati sia quello che ti aspetti
-
-        EXTRA:
-        / Dai un feedback sulla risposta al momento del click (corretta o sbagliata)
-        / Visualizza una domanda alla volta in sequenza piuttosto che tutte assieme in forma di lista
-        / Permetti all'utente di selezionare la difficoltà del quiz prima di iniziare e il numero di domande che desidera ricevere.
-        ( Se hai implementato l'applicazione usando l'URL fornito, puoi ottenere i dati che ti servono in modo semplice, 
-        usando query parameters in questo modo: https://opentdb.com/api.php?amount=10&category=18&difficulty=easy e modificarne il numero di domande e difficoltà )
-    
-        /* NON DIMENTICARE...
-          di fare commit & push del codice regolarmente sulla tua repository GitHub e di condividerla con i tuoi colleghi
-        */
 
 
 
 
 
 
-// TIPS:
-
-        // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
-        // Per ogni domanda, crea un container e incorporale tutte all'interno. 
-        // Crea poi dei radio button
-        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-        // con le risposte corrette e incorrette come opzioni
-        // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale) 
-//
-// SE MOSTRI UNA DOMANDA ALLA VOLTA:
-// Mostra la prima domanda con il testo e i radio button.
-// Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
-// salvando le risposte dell'utente in una variabile
 
 
-// Come calcolare il risultato? Hai due strade:
-// Se stai mostrando tutte le domande nello stesso momento, controlla semplicemente se i radio button selezionati sono === correct_answer
-// Se stai mostrando una domanda alla volta, aggiungi semplicemente un punto alla variabile del punteggio che hai precedentemente creato SE la risposta selezionata è === correct_answer
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
 
-// BUON LAVORO 💪🚀
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
